8e8da7a365295322da0680c9249890e4f378cbff
SeounBroadCast <142962040+SeounBroadCast@users.noreply.github.com>
2023-10-04 13:15:59 +0900

    Update loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import styles from "./loading.module.css";
 
 const LoadingScreen = () => {
@@ -13,50 +13,25 @@ const LoadingScreen = () => {
 
   return (
     <>
-      <div className={styles.loading}>
-        <svg
-          className={styles.pl}
-          viewBox="0 0 200 200"
-          width="200"
-          height="200"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <linearGradient id="pl-grad1" x1="1" y1="0.5" x2="0" y2="0.5">
-              <stop offset="0%" stopColor="hsl(313,90%,55%)" />
-              <stop offset="100%" stopColor="hsl(223,90%,55%)" />
-            </linearGradient>
-            <linearGradient id="pl-grad2" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="0%" stopColor="hsl(313,90%,55%)" />
-              <stop offset="100%" stopColor="hsl(223,90%,55%)" />
-            </linearGradient>
-          </defs>
-          <circle
-            className={styles.pl__ring}
-            cx="100"
-            cy="100"
-            r="82"
-            fill="none"
-            stroke="url(#pl-grad1)"
-            strokeWidth="36"
-            strokeDasharray="0 257 1 257"
-            strokeDashoffset="0.01"
-            strokeLinecap="round"
-            transform="rotate(-90,100,100)"
-          />
-          <line
-            className={styles.pl__ball}
-            stroke="url(#pl-grad2)"
-            x1="100"
-            y1="18"
-            x2="100.01"
-            y2="182"
-            strokeWidth="36"
-            strokeDasharray="1 165"
-            strokeLinecap="round"
-          />
-        </svg>
-      </div>
+      {document.body.className.split(" ").pop() === "dark-mode-variables" ? (
+        <>
+          <main>
+            <div className={styles.skeleton_main_dark}></div>
+          </main>
+          <div className="right-section">
+            <div className={styles.skeleton_right_dark}></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <main>
+            <div className={styles.skeleton_main}></div>
+          </main>
+          <div className="right-section">
+            <div className={styles.skeleton_right}></div>
+          </div>
+        </>
+      )}
     </>
   );
 };
